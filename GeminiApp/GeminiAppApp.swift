f07726df12eec2647c11518a40fbafe8276b405a
Tobias Fu <fuz.sh0710@gmail.com>
2025-11-28 08:39:25 -0500

    Initial Commit

---
+++ GeminiApp/GeminiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiAppApp.swift
+//  GeminiApp
+//
+//  Created by Tobias Fu on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
